7ffefd79ed28e8bba80c218955c32d5769a90bc1
kociumbas <kociumbas@outlook.com>
2016-02-16 01:20:04 +0100

    ShipmentCalculator

---
+++ shipmentCalculator.py
@@ -0,0 +1,26 @@
+﻿totalCharge = 0
+country = ""
+orderTotal = 0
+province = ""
+GST = .05
+HAR = .13
+PRO = .06
+
+orderTotal = float(input ("What is your order total? "))
+print (orderTotal)
+country = input ("What country are you from? ").upper()
+if country.upper() == "CANADA" :
+    province = input ("Which province in Canada? ").upper()
+    if country.upper() == "CANADA" and province.upper() == "ALBERTA" :
+        totalCharge = orderTotal + orderTotal * GST
+    elif country.upper() == "CANADA" and province.upper() == "ONTARIO" or province.upper() == "NEW BRUNSWICK" or province.upper() == "NOVA SCOTIA"  :
+        totalCharge = orderTotal + orderTotal * HAR
+    else:
+        totalCharge = orderTotal * PRO + orderTotal * GST + orderTotal
+else:
+    totalCharge = orderTotal
+
+print ("The total order plus taxex is: ") 
+print (totalCharge)
+
+print("Your total including taxes comes to $%.2f " % totalCharge)
